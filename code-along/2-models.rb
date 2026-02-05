1ef04a38c3517cd18c3c3763e596e07e27f38de1
gargbydesign <amit.garg@kellogg.northwestern.edu>
2026-02-05 02:23:30 +0000

    code along for models

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -15,13 +15,52 @@ Company.destroy_all
 # 1b. check out the model file
 
 # 2. insert new rows in companies table
+new_company = Company.new
+new_company["name"] = 'Apple'
+new_company["city"] = 'Cupertino'
+new_company["state"] = 'CA'
+new_company["url"] = 'https://apple.com'
+new_company.save
+
+
+#
+new_company = Company.new
+new_company["name"] = 'Amazon'
+new_company["city"] = 'Seattle'
+new_company["state"] = 'WA'
+new_company["url"] = 'https://amazon.com'
+new_company.save
+
+new_company = Company.new
+new_company["name"] = 'Anthropic'
+new_company["city"] = 'San Francisco'
+new_company["state"] = 'CA'
+new_company["url"] = 'https://anthropic.com'
+new_company.save
+
+puts "Companies: #{Company.all.count}"
+
 
 # 3. query companies table to find all row with California company
+cali_companies = Company.where({"state" => "CA"})
+#p cali_companies
+puts "California companies: #{cali_companies.count}"
+
 
 # 4. query companies table to find single row for Apple
+apple = Company.find_by({"name" => "Apple"})
+p apple
 
 # 5. read a row's column value
+puts apple["url"]
+
 
 # 6. update a row's column value
+apple ["url"] = "https://www.apple.com"
+apple.save
+p apple
+
+company = Company.find_by({"name" => "Apple"})
+puts company["url"]
 
 # 7. delete a row
